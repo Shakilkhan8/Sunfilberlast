d6446d9806009c0b04ec9a5c0c09ca51f527fa78
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-10-12 20:31:00 +0500

    Add files via upload

--- sale_order_list_changs/models/sale_list_view_changes.py
+++ sale_order_list_changs/models/sale_list_view_changes.py
@@ -4,6 +4,10 @@ class SaleOrderListView(models.Model):
     _inherit = 'sale.order'
 
     state = fields.Selection(selection_add=[('dispatch', 'Dispatch')])
+    order_create_date = fields.Datetime('Create Date')
+    expect_date = fields.Date('Expected Date')
 
     def action_dispatch(self):
         self.state = 'dispatch'
+
+
