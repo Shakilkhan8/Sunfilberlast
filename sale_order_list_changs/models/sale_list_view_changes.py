702910bf4a20b46669998d4bffe2cecbdd0c66cd
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-10-17 16:42:38 +0500

    Add files via upload

--- sale_order_list_changs/models/sale_list_view_changes.py
+++ sale_order_list_changs/models/sale_list_view_changes.py
@@ -3,9 +3,16 @@ from odoo import api, fields, models
 class SaleOrderListView(models.Model):
     _inherit = 'sale.order'
 
-    state = fields.Selection(selection_add=[('dispatch', 'Dispatch')])
-    order_create_date = fields.Datetime('Create Date')
-    expect_date = fields.Date('Expected Date')
+    receive_date = fields.Datetime('Receive Date')
+    expect_date = fields.Datetime('Expected Date')
+    state = fields.Selection([
+        ('draft', 'Quotation'),
+        ('dispatch', 'Dispatch'),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sales Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+    ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
 
     def action_dispatch(self):
         self.state = 'dispatch'
